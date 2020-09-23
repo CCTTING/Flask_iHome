071115b6a17c3b74b234379baa0af1e068a66a89
caoting <caoting0403@163.com>
2020-09-23 11:19:47 +0800

    ihome

--- ihome/api_1_0/verify_code.py
+++ ihome/api_1_0/verify_code.py
@@ -1,13 +1,39 @@
 # coding:utf-8
 
 from . import api
+from ihome.utils.captcha.captcha import captcha
+from ihome import redis_store, constants
+from flask import current_app, jsonify, make_response
+from ihome.utils.response_code import RET
+
 
 @api.route("/get_image_codes/<image_code_id>")
 def get_image_code(image_code_id):
     """
     前端获取验证码
-    :return:    验证码图片
+    :return:    验证码图片  异常：返回json
     """
     # 业务逻辑处理
     # 生产验证码图片
-    #
+    # 名字，真是文本，图片数据
+    name, text, image_data = captcha.generate_captcha()
+
+    # 将验证码真实值与编号保存到redis中，设置有效期
+    # redis数据类型：字符串 列表  哈希  set
+    # "key"：xxx
+    # 使用哈希维护有效期的时候只能整体设置
+    # image_codes:{"":"","":""}
+
+    # redis_store.set("image_code_%s" % image_code_id, text)
+    # redis_store.expire("image_code_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES)
+    #                       名字                          有效期                             记录值
+    try:
+        redis_store.setex("image_code_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+    except Exception as e:
+        # 记录日志
+        current_app.logger.error(e)
+        return jsonify(error=RET.DBERR, errmsg="save image code id failed")
+    # 返回图片
+    resp = make_response(image_data)
+    resp.headers["Content-Type"] = "image/jpg"
+    return resp
--- ihome/utils/captcha/captcha.py
+++ ihome/utils/captcha/captcha.py
@@ -1,11 +1,7 @@
-# coding:utf-8
-
-# refer to `https://bitbucket.org/akorn/wheezy.captcha`
-
 import random
 import string
 import os.path
-from cStringIO import StringIO
+from io import BytesIO
 
 from PIL import Image
 from PIL import ImageFilter
@@ -67,7 +63,7 @@ class Captcha(object):
 
     def initialize(self, width=200, height=75, color=None, text=None, fonts=None):
         # self.image = Image.new('RGB', (width, height), (255, 255, 255))
-        self._text = text if text else random.sample(string.uppercase + string.uppercase + '3456789', 4)
+        self._text = text if text else random.sample(string.ascii_uppercase + string.ascii_uppercase + '3456789', 4)
         self.fonts = fonts if fonts else \
             [os.path.join(self._dir, 'fonts', font) for font in ['Arial.ttf', 'Georgia.ttf', 'actionj.ttf']]
         self.width = width
@@ -204,9 +200,9 @@ class Captcha(object):
         image = self.curve(image)
         image = self.noise(image)
         image = self.smooth(image)
-        name = "".join(random.sample(string.lowercase + string.uppercase + '3456789', 24))
+        name = "".join(random.sample(string.ascii_lowercase + string.ascii_lowercase + '3456789', 24))
         text = "".join(self._text)
-        out = StringIO()
+        out = BytesIO()
         image.save(out, format=fmt)
         if path:
             image.save(os.path.join(path, name), fmt)
@@ -220,4 +216,4 @@ class Captcha(object):
 captcha = Captcha.instance()
 
 if __name__ == '__main__':
-    captcha.generate_captcha()
+    print(captcha.generate_captcha())
